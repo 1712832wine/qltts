a19bc59cf21acfc86e258b97e190a4368dc325d9
1712832wine <nguyenhuutri31081999nht@gmail.com>
2022-05-29 18:57:37 +0700

    ok

--- src/API/apis.js
+++ src/API/apis.js
@@ -1,20 +1,22 @@
 import axios from "axios";
 
+const url = 'https://manage-intern.herokuapp.com'
+
 export const apis = {
     getInterns: (page) => {
-        return axios.get(`http://127.0.0.1:8000/interns?page=${page}`)
+        return axios.get(`${url}/interns?page=${page}`)
     },
     viewIntern: (id) => {
-        return axios.get(`http://127.0.0.1:8000/interns/${id}`)
+        return axios.get(`${url}/interns/${id}`)
     },
     deleteIntern: (id) => {
-        return axios.delete(`http://127.0.0.1:8000/interns/${id}`)
+        return axios.delete(`${url}/interns/${id}`)
 
     },
     createIntern: (values) => {
-        return axios.post('http://127.0.0.1:8000/interns', values)
+        return axios.post(`${url}/interns`, values)
     },
     editIntern: (id, values) => {
-        return axios.put(`http://127.0.0.1:8000/interns/${id}`, values)
+        return axios.put(`${url}/interns/${id}`, values)
     }
 };
